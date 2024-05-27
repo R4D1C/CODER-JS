d38045b21a743a608d4f28885e2d7e46d24e0980
Mascetti Joaquin <serraojoaquin@gmail.com>
2024-05-27 14:14:49 -0300

    Segundo commit

--- SCRIPTS/scripts.js
+++ SCRIPTS/scripts.js
@@ -11,16 +11,26 @@ n es el numero total de pagos
 
 */
 // No se si sea incorrecto poner la Ñ, así que por las dudas la cambio por una N
-function Calculadora (monto, interes, anos) {
-    // Interes pide que se ingrese en porcentajes, Con este calculo lo paso a decimales y lo divido por los meses de un año
-    const intereses = interes / 100 / 12;
-    const pagos = anos * 12;
-    const x = Math.pow(1 + intereses, pagos);
-    const pagoMensual = (monto * x * intereses) / (x - 1);
 
-    // Muestro el resultado en consola
-    console.log(`El pago mensual es de: ${pagoMensual.toFixed(2)}`);
+    const monto1 = parseInt(prompt(`Por favor ingrese el monto del crédito`));
+    const interes1 = parseInt(prompt(`Ingrese el índice interés en porcentaje`));
+    const anos1 = parseInt(prompt(`Ingrese los años que tomará pagar el crédito`));
+
+function Calculadora (monto, interes, anos) {
+
+    if (monto > 0 && interes > 0 && monto > 0) {
+        // Interes pide que se ingrese en porcentajes, Con este calculo lo paso a decimales y lo divido por los meses de un año
+        const intereses = interes / 100 / 12;
+        const pagos = anos * 12;
+        const x = Math.pow(1 + intereses, pagos);
+        const pagoMensual = (monto * x * intereses) / (x - 1);
+
+        // Muestro el resultado en consola
+        console.log(`El pago mensual es de: ${pagoMensual.toFixed(2)}`);
+    } else {
+        alert(`Por favor ingrese valores válidos`);
+    }
 }
 
 // Llamo a la función y relleno los parámetros
-Calculadora(210000, 17, 4);
+Calculadora(monto1, interes1, anos1);
